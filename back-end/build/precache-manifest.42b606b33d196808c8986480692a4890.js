9baf819a1d30b27914b6de9466611a157be8a68e
yacov fredman <jacov141@gmail.com>
2020-12-23 09:39:14 +0200

    made build with static url of google cloud

---
+++ back-end/build/precache-manifest.42b606b33d196808c8986480692a4890.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf6ddd870714f2fc8d6aa02bc7de7b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e3e229a86a491f0a9cad",
+    "url": "./static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "2da1af406de5c20c0476",
+    "url": "./static/css/main.10bc000e.chunk.css"
+  },
+  {
+    "revision": "e3e229a86a491f0a9cad",
+    "url": "./static/js/2.d5edb5d7.chunk.js"
+  },
+  {
+    "revision": "adc206df58a1d21a4525b50ff2c3f1ff",
+    "url": "./static/js/2.d5edb5d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da1af406de5c20c0476",
+    "url": "./static/js/main.37bf092d.chunk.js"
+  },
+  {
+    "revision": "6ff08f4d2bb391b3889e",
+    "url": "./static/js/runtime-main.2ef79076.js"
+  },
+  {
+    "revision": "42871be003a55bb3e996be9ea36ff7c1",
+    "url": "./static/media/BackGroundLeftRoad.42871be0.svg"
+  },
+  {
+    "revision": "3f3f06a1103f888ebe8ef81189351d2e",
+    "url": "./static/media/avigul-bold-fm.3f3f06a1.woff"
+  },
+  {
+    "revision": "78ec4dc8f95383b2bbe00f12e936f791",
+    "url": "./static/media/avigul-bold-fm.78ec4dc8.woff2"
+  },
+  {
+    "revision": "4f1634fd9f6f8f970ab2d504db25c96f",
+    "url": "./static/media/backGroundWhatsApp1.4f1634fd.svg"
+  }
+]);
--- front-end/src/utils/StaticData.js
+++ front-end/src/utils/StaticData.js
@@ -1,11 +1,13 @@
 
 
+
+export const baseUrl = "https://achraiut-test.el.r.appspot.com/";
 // export const baseUrl = "https://story.mmb.org.il/server1/";
 // export const baseUrl = "https://story.mmb.org.il/server1/api/";
 
 
 
-export const baseUrl = "http://localhost:5000/";
+// export const baseUrl = "http://localhost:5000/";
 // export const baseUrl = "http://story.mmb.org.il/server1/";
 // export const expTokenTime = 1000 * 60 * 60; // one hour
 
