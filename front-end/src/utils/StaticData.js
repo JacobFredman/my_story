d26faf2ffee8d57917802bcdaed81d585bfcd236
yacov fredman <jacov141@gmail.com>
2021-01-12 20:03:07 +0200

    base url and packet.json are localhost:5000

--- front-end/src/utils/StaticData.js
+++ front-end/src/utils/StaticData.js
@@ -5,8 +5,8 @@
 
 
 
-export const baseUrl = "https://my-story-pro.ew.r.appspot.com/api/";
-// export const baseUrl = "http://localhost:5000/api/";
+// export const baseUrl = "https://my-story-pro.ew.r.appspot.com/api/";
+export const baseUrl = "http://localhost:5000/api/";
 
 
 
